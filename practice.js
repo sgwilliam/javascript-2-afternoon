5d74305b27dfa556aa083b0417fb4600b253b830
sgwilliam <sdgwills@gmail.com>
2019-03-20 13:32:55 -0600

    initial commit

--- practice.js
+++ practice.js
@@ -14,9 +14,12 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+me = {
+  name: 'Spencer',
+  age: 20
+}
 
-
+alert(me.name)
 
 ////////// PROBLEM 2 //////////
 
@@ -25,7 +28,14 @@
   Have the values to those keys be your favorite thing in that category.
 */
 
-//Code here
+favoriteThings = {
+  band: 'Twenty One Pilot',
+  food: 'Steak',
+  person: 'Your mom',
+  book: 'The Way of Kings',
+  movie: 'How to Train Your Dragon',
+  holiday: 'Christmas'
+}
 
 
 
@@ -33,7 +43,8 @@
   After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 */
 
-//Code here
+favoriteThings.car = 'Tesla'
+favoriteThings.brand = 'DevMountain'
 
 
 
@@ -41,8 +52,8 @@
   Now change the value of the food key in your favoriteThings object to be 'Chicken Nuggets' and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
 */
 
-//Code here
-
+favoriteThings.food = 'Chicken Nuggets'
+favoriteThings.book = 'Harry Potter'
 
 
 ////////// PROBLEM 3 //////////
@@ -55,17 +66,19 @@
   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack. 
 */
 
-//Code here
+backPack = {
 
+}
 
+let item = "firstPocket"
+backPack.firstPocket = 'chapstick'
+backPack.color = 'camo'
 
 /*
   After you do the above, alert your entire backPack object.
 */
 
-//Code here
-
-
+alert(backPack)
 
 /*
 You probably noticed that it just alerted [object Object].
@@ -73,7 +86,7 @@ Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. 
 */
 
-//Code here
+console.log(backPack)
 
 
 
@@ -96,9 +109,8 @@ var user2 = {
   Make that change without modifying the original object code above.
 */
 
-//Code Here
-
-
+user2.name = 'Tyler S. McGinnis'
+user2.email = 'tyler.mcginnis@devmounta.in'
 
 
 
@@ -119,7 +131,9 @@ var user2 = {
   Create an empty object called methodCollection.
 */
 
-//Code Here
+methodCollection = {
+
+}
 
 
 
@@ -128,7 +142,7 @@ var user2 = {
   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console. 
 */
 
-//Code Here
+
 
 
 
